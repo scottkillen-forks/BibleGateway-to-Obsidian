scottkillen-forks/BibleGateway-to-Obsidian

commit 2ba1fc9a2c4201e00578118839e3e818981eed72

scripting for verse splitting, master files, and formatting

bg2obs.sh
@@ -17,7 +17,7 @@
 #----------------------------------------------------------------------------------
 
 # OPTIONS
-translation="WEB" # Set translation
+translation="ESV" # Set translation
 boldwords="false" # Set 'true' for bolding words of Jesus
 headers="false" # Set 'true' for including editorial headers
 
@@ -53,27 +53,12 @@ abbarray=(Gen Exod Lev Num Deut Josh Judg Ruth "1 Sam" "2 Sam" "1 Kings" "2 King
 ((next_chapter=chapter+1))
 
 # Exporting
-  export_prefix="${abbreviation}-" # Setting the first half of the filename
+  export_prefix="${book} " # Setting the first half of the filename
 
-  if (( ${chapter} < 10 )); then # Making sure single digit numbers are preceded by a 0 for proper sorting
-    #statements
-    export_number="0${chapter}"
-  else
-    export_number=${chapter}
-  fi
+  export_number=${chapter}
 
 filename=${export_prefix}$export_number # Setting the filename
 
-# Navigation in the note
-  if (( ${prev_chapter} < 10 )); then # Turning singe into double digit numbers
-    #statements
-    prev_chapter="0${prev_chapter}"
-  fi
-
-  if (( ${next_chapter} < 10 )); then # Turning singe into double digit numbers
-    #statements
-    next_chapter="0${next_chapter}"
-  fi
 
   prev_file=${export_prefix}$prev_chapter # Naming previous and next files
   next_file=${export_prefix}$next_chapter
@@ -94,13 +79,13 @@ filename=${export_prefix}$export_number # Setting the filename
   fi
 
   if ${boldwords} -eq "true" && ${headers} -eq "false"; then
-    text=$(ruby bg2md.rb -e -c -b -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
+    text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -b -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
   elif ${boldwords} -eq "true" && ${headers} -eq "true"; then
-    text=$(ruby bg2md.rb -c -b -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
+    text=$(ruby BibleGateway-to-Markdown/bg2md.rb -c -b -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
   elif ${boldwords} -eq "false" && ${headers} -eq "true"; then
-    text=$(ruby bg2md.rb -e -c -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
+    text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
   else
-    text=$(ruby bg2md.rb -e -c -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
+    text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
   fi
 
 
@@ -114,7 +99,7 @@ filename=${export_prefix}$export_number # Setting the filename
 
 
   # Export
-  echo -e $export >> "$filename.md"
+  echo -e $text >> "$filename.md"
 
   # Creating a folder
 
@@ -122,15 +107,15 @@ filename=${export_prefix}$export_number # Setting the filename
 
   if (( $actual_num < 10 )); then
     #statements
-    actual_num="0${actual_num}"
+    actual_num="${actual_num}"
   else
     actual_num=$actual_num
   fi
 
-  folder_name="${actual_num} - ${book}" # Setting the folder name
+  folder_name="${book}" # Setting the folder name
 
   # Creating a folder for the book of the Bible it not existing, otherwise moving new file into existing folder
-  mkdir -p "./Scripture (${translation})/${folder_name}"; mv "${filename}".md './Scripture ('"${translation}"')/'"${folder_name}"
+  mkdir -p "./${translation}/${folder_name}"; mv "${filename}".md "${translation}/${folder_name}"
 
 
 done # End of the book exporting loop
@@ -139,7 +124,7 @@ done # End of the book exporting loop
   overview_file="links: [[The Bible]]\n# ${book}\n\n[[${abbreviation}-01|Start Reading →]]"
   echo -e $overview_file >> "$book.md"
   #mkdir -p ./Scripture ("${translation}")/"${folder_name}"; mv "$book.md" './Scripture ('"${translation}"')/'"${folder_name}"
-  mv "$book.md" './Scripture ('"${translation}"')/'"${folder_name}"
+  mv "$book.md" "${translation}/${folder_name}"
 
   done
 

.gitmodules
@@ -1,0 +1,3 @@
+[submodule "BibleGateway-to-Markdown"]
+	path = BibleGateway-to-Markdown
+	url = https://github.com/jgclark/BibleGateway-to-Markdown

masterfiles.sh
@@ -1,0 +1,18 @@
+#!/bin/bash
+for book in ESV/*/;
+do
+  bookname=`basename "$book"`
+  bookname="${bookname:5}"
+  for chapter in "${book}"*/;
+  do
+    chaptername=`basename "$chapter"`
+    echo "[[$chaptername]]" >> "$book$bookname.md"
+    for verse in "${chapter}"*.md;
+    do
+      versefile=`basename "$verse"`
+      verse="`echo $versefile | rev | cut -c4- | rev`"
+      echo "$verse"
+      echo "[[$verse]]" >> "$chapter$chaptername.md"
+    done;
+  done;
+done;

rename-folders.sh
@@ -1,0 +1,10 @@
+#!/bin/bash
+bookarray=(Genesis Exodus Leviticus Numbers Deuteronomy Joshua Judges Ruth "1 Samuel" "2 Samuel" "1 Kings" "2 Kings" "1 Chronicles" "2 Chronicles" Ezra Nehemiah Esther Job Psalm Proverbs Ecclesiastes "Song of Solomon" Isaiah Jeremiah Lamentations Ezekiel Daniel Hosea Joel Amos Obadiah Jonah Micah Nahum Habakkuk Zephaniah Haggai Zechariah Malachi Matthew Mark Luke John Acts Romans "1 Corinthians" "2 Corinthians" Galatians Ephesians Philippians Colossians "1 Thessalonians" "2 Thessalonians" "1 Timothy" "2 Timothy" Titus Philemon Hebrews James "1 Peter" "2 Peter" "1 John" "2 John" "3 John" Jude Revelation)
+
+for (( n=0; n<=65; n++))
+do
+  number=$(( $n + 1))
+  numberstr="0${number}"
+  numberstr=${numberstr: -2};
+  mv "ESV/${bookarray[$n]}/" "ESV/${numberstr} - ${bookarray[$n]}/";
+done;

versesplit.sh
@@ -1,0 +1,26 @@
+#!/bin/bash
+for folder in ESV/*/;
+do
+  for file in "${folder}"*.md;
+  do
+    filename=${file##*/};
+    bookchapter="`echo $filename | rev | cut -c4- | rev`"
+    echo "![[${folder$bookchapter}]]" >> "${folder}/${folder:5}.md"
+    echo "$folder$bookchapter.md"
+    while IFS= read -r line
+    do
+      if [[ ${line:0:1} == "v" ]];
+      then
+        verse="`grep -o '^v[0-9]*' <<<$line`"
+        text="`cut -d ' ' -f2- <<<$line`"
+        num="${verse:1}"
+        if [ ! -d "$folder$bookchapter" ]; then
+          mkdir "$folder$bookchapter"
+        fi
+        echo "$text" > "$folder$bookchapter/$bookchapter.$num.md"
+        echo "![[$bookchapter.$num]]" >> "$folder$bookchapter/$bookchapter.md"
+      fi
+    done < "$folder$bookchapter.md"
+    rm "$folder$bookchapter.md"
+  done;
+done;

commit e9d751d5da83677ffb3f35b152f204c88ccf20db

Restore navigation links to chapter files   Error introduced in 2ba1fc9a2c4201e00578118839e3e818981eed72

bg2obs.sh
@@ -79,7 +79,7 @@ filename=${export_prefix}$export_number # Setting the filename
 
 
   # Export
-  echo -e $text >> "$filename.md"
+  echo -e $export >> "$filename.md"
 
   # Creating a folder
 